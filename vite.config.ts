516a7bfdae7705cf8982bcdc6f862b32d6fc17c3
sez <bozdemirsezgin1@gmail.com>
2026-02-24 14:20:50 +0200

    remove favicon link and update Vite config with new plugins and settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import electron from "vite-plugin-electron/simple";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const isProd = command === "build";
+  return {
+    base: "./",
+    plugins: [
+      react(),
+      tailwindcss(),
+      tanstackRouter(),
+      electron({
+        main: { entry: "electron/main/index.ts" },
+        preload: { input: "electron/preload/index.ts" },
+        renderer: {},
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: true,
+      strictPort: true,
+    },
+    build: {
+      outDir: path.join(__dirname, "dist"),
+      cssCodeSplit: true,
+      drop: isProd ? ["console", "debugger"] : [],
+      rollupOptions: {
+        output: {
+          sourcemap: false,
+          entryFileNames: "js/[name].[hash].js",
+          chunkFileNames: "js/[name].[hash].js",
+          assetFileNames: "[ext]/[name].[hash].[ext]",
+        },
+      },
+    },
+  };
+});
